fee0367bed9751039284d945fa07656acd01b251
Shahajahan15 <mdrezaali1@gmail.com>
2019-09-14 19:03:25 +0600

    Math class (Remake)

---
+++ MathClass.java
@@ -0,0 +1,22 @@
+package oop;
+
+public class MathClass {
+    
+    public static void main(String[] args) {
+        
+        int x = Math.abs(-10);
+        System.out.println("Absulate value : "+x);
+        System.out.println("Absulate value : "+Math.abs(-10.4));
+        System.out.println("Square root value : "+Math.sqrt(36.0));
+        System.out.println("Power value : "+Math.pow(2, 3));
+        System.out.println("Pie Vslue : "+Math.PI);
+        System.out.println("Log value 1 : "+Math.log(1));
+        System.out.println("Log value 2 : "+Math.log(2));
+        System.out.println("Max value 2 : "+Math.max(3.6, 2.9));
+        System.out.println("Min value 2 : "+Math.min(3.6, 11.9));
+        System.out.println("Ceilin value : "+Math.ceil(5.4));
+        System.out.println("Floor value : "+Math.floor(5.4));
+    }
+        
+    
+}
